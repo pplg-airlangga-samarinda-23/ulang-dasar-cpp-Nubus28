ec269cd0525f0200a57403a997c5cca335bdfd69
batre9080@gmail.com <batre9080@gmail.com>
2024-07-31 12:39:03 +0800

    Selesai tugas

--- kode1.cpp
+++ kode1.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
+void cetakData(string nama, string nisn, string jurusan, float tinggiBadan) {
+    cout << "Nama: " << nama << endl;
+    cout << "NISN: " << nisn << endl;
+    cout << "Jurusan: " << jurusan << endl;
+    cout << "Tinggi Badan: " << tinggiBadan << " cm" << endl;
+}
 
 int main() {
-    cout << "Selamat Datang di SMK TI Airlangga" << endl;
+    string nama, nisn, jurusan;
+    float tinggiBadan;
+
+    cout << "Masukkan nama siswa: ";
+    getline(cin, nama);
+
+    cout << "Masukkan NISN siswa: ";
+    cin >> nisn;
+
+    cout << "Masukkan jurusan siswa: ";
+    cin.ignore(); // Membersihkan buffer input
+    getline(cin, jurusan);
+
+    cout << "Masukkan tinggi badan siswa (cm): ";
+    cin >> tinggiBadan;
+
+    cetakData(nama, nisn, jurusan, tinggiBadan);
+
     return 0;
 }
